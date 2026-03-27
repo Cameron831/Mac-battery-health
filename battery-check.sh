5ecab7f9fe61459c3cfafaaef7687a84de49f68e
Cameron Harris <camharris831@gmail.com>
2026-03-27 13:28:10 -0700

    changed to printf

--- battery-check.sh
+++ battery-check.sh
@@ -6,7 +6,7 @@ RED="\033[0;31m"
 NC="\033[0m"
 THRESHOLD=80
 
-echo -e "Starting battery check...\n"
+printf "Starting battery check...\n"
 
 # AppleSmartBattery as XML
 BATTERY_XML=$(ioreg -rc AppleSmartBattery -a)
@@ -15,18 +15,19 @@ BATTERY_XML=$(ioreg -rc AppleSmartBattery -a)
 DesignCapacity=$(echo "$BATTERY_XML" | plutil -extract 0.DesignCapacity raw -)
 AppleRawMaxCapacity=$(echo "$BATTERY_XML" | plutil -extract 0.AppleRawMaxCapacity raw -)
 
-echo "Design Capacity:      ${DesignCapacity} mAh"
-echo "Current Max Capacity: ${AppleRawMaxCapacity} mAh"
+printf "%-22s %s mAh\n" "Design Capacity:" "$DesignCapacity"
+printf "%-22s %s mAh\n" "Current Max Capacity:" "$AppleRawMaxCapacity"
 
 # Calculate health percentage as int
 health=$(awk "BEGIN { printf \"%.2f\", ($AppleRawMaxCapacity / $DesignCapacity) * 100 }")
 health_int=${health%.*}
 
-echo "Battery Health:       ${health}%"
+printf "%-22s %s%%\n" "Battery Health:" "$health"
 
 # Compare and print with color
 if (( health_int < THRESHOLD )); then
-  echo -e "Status:              ${RED}FAIL${NC}\n"
+  printf "%-22s ${RED}%s${NC}\n" "Status:" "FAIL"
 else
-  echo -e "Status:              ${GREEN}PASS${NC}\n"
+  printf "%-22s ${GREEN}%s${NC}\n" "Status:" "PASS"
 fi
+echo
